13bff4f76ef2bdd8ca1362cd35b493245d0b44b0
amanchhatbar <32755035+amanchhatbar@users.noreply.github.com>
2025-03-23 16:18:02 -0500

    practice

--- LeetCodePractice/NeetCodePract/SlidingWindow/PermutationString.cs
+++ LeetCodePractice/NeetCodePract/SlidingWindow/PermutationString.cs
@@ -4,18 +4,18 @@ public class PermutationString
 {
     public PermutationString()
     {
-        Console.WriteLine(CheckInclusion("adc", "dcda"));
+        Console.WriteLine(CheckInclusion("hello", "ooolleoooleh"));
     }
     private bool CheckInclusion(string s1, string s2)
     {
         if (s1.Length > s2.Length) return false;
-        var alphabetCharArray = Enumerable.Repeat('0', 26).ToArray();
+        var alphabetCharArray = Enumerable.Repeat(0, 26).ToArray();
         var s1MarkedOnes = MarkOnes(alphabetCharArray, s1);
         var windowLength = s1.Length;
         var left = 0;
         while (left <= s2.Length - windowLength)
         {
-            alphabetCharArray = Enumerable.Repeat('0', 26).ToArray();
+            alphabetCharArray = Enumerable.Repeat(0, 26).ToArray();
             var subString = s2.Substring(left, windowLength);
             var s2MarkedOnes = MarkOnes(alphabetCharArray, subString);
             if (string.Compare(s1MarkedOnes, s2MarkedOnes) == 0)
@@ -28,14 +28,14 @@ public class PermutationString
     }
 
 
-    private string MarkOnes(char[] alphabetArray, string stringArray)
+    private string MarkOnes(int[] alphabetArray, string stringArray)
     {
         foreach (var input in stringArray)
         {
             var index = input - 'a';
-            alphabetArray[index] = '1';
+            alphabetArray[index]++;
         }
 
-        return (new string(alphabetArray));
+        return (string.Join("", alphabetArray));
     }
 }
--- LeetCodePractice/obj/Debug/net9.0/LeetCodePractice.AssemblyInfo.cs
+++ LeetCodePractice/obj/Debug/net9.0/LeetCodePractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodePractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ad53a1dcb0467a29a988ccdea930b2d8dd637c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744e1cba3a65d26e9ba4c36b0f1bdb4d9b1b0153")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodePractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodePractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
